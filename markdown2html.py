df54a02c1fde5dba706ea70cbd83f3d79cf07e6a
Huangdi-599 <adebayoh76@gmail.com>
2023-07-16 16:52:38 -0700

    fid

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 """
-markdown2html.py
-
-Converts Markdown file to HTML file with additional parsing.
-
-Usage: ./markdown2html.py <input_file> <output_file>
+Markdown to HTML Converter
 """
 
 import sys
@@ -15,13 +10,8 @@ import hashlib
 
 def convert_markdown_to_html(input_file, output_file):
     """
-    Converts Markdown file to HTML file with additional parsing.
-
-    Args:
-        input_file (str): Path to the input Markdown file.
-        output_file (str): Path to the output HTML file.
+    Convert Markdown content to HTML and write to output file
     """
-
     if not os.path.isfile(input_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
@@ -31,15 +21,35 @@ def convert_markdown_to_html(input_file, output_file):
         markdown_content = file.read()
 
     # Convert Markdown headings to HTML headings
-    html_content = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', markdown_content)
+    html_content = re.sub(r'^#\s(.+)$', r'<h1>\1</h1>', markdown_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^##\s(.+)$', r'<h2>\1</h2>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^###\s(.+)$', r'<h3>\1</h3>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^####\s(.+)$', r'<h4>\1</h4>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^#####\s(.+)$', r'<h5>\1</h5>', html_content, flags=re.MULTILINE)
+    html_content = re.sub(r'^######\s(.+)$', r'<h6>\1</h6>', html_content, flags=re.MULTILINE)
+
+    # Convert Markdown unordered lists to HTML lists
+    html_content = re.sub(r'^-\s(.+)$', r'<ul>\n<li>\1</li>\n</ul>', html_content, flags=re.MULTILINE)
+
+    # Convert Markdown ordered lists to HTML lists
+    html_content = re.sub(r'^\*\s(.+)$', r'<ol>\n<li>\1</li>\n</ol>', html_content, flags=re.MULTILINE)
+
+    # Convert Markdown paragraphs to HTML paragraphs
+    html_content = re.sub(r'^([^\-*#].*)$', r'<p>\n\1\n</p>', html_content, flags=re.MULTILINE)
+
+    # Convert Markdown bold syntax to HTML bold tags
+    html_content = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', html_content)
     html_content = re.sub(r'__(.*?)__', r'<em>\1</em>', html_content)
+
+    # Convert custom syntax: [[content]] to MD5 hash (lowercase)
     html_content = re.sub(r'\[\[(.*?)\]\]', lambda match: hashlib.md5(match.group(1).encode()).hexdigest(), html_content)
+
+    # Convert custom syntax: ((content)) to remove all occurrences of 'c' (case insensitive)
     html_content = re.sub(r'\(\((.*?)\)\)', lambda match: match.group(1).replace('c', ''), html_content, flags=re.IGNORECASE)
 
     # Write the HTML content to the output file
     with open(output_file, 'w') as file:
         file.write(html_content)
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -50,4 +60,5 @@ if __name__ == "__main__":
     output_file = sys.argv[2]
 
     convert_markdown_to_html(input_file, output_file)
+
     sys.exit(0)
